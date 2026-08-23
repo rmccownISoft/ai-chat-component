595482789c14ee70af9ba5a3bc687299da5f665a
rmccownISoft <60158596+rmccownISoft@users.noreply.github.com>
2026-08-22 22:36:45 -0500

    chore: try migrating to isoft eslint/prettier

--- eslint.config.js
+++ eslint.config.js
@@ -1,29 +1,35 @@
-import prettier from 'eslint-config-prettier';
-import path from 'node:path';
-import { includeIgnoreFile } from '@eslint/compat';
-import js from '@eslint/js';
-import svelte from 'eslint-plugin-svelte';
-import { defineConfig } from 'eslint/config';
-import globals from 'globals';
-import ts from 'typescript-eslint';
-import svelteConfig from './svelte.config.js';
+import { fileURLToPath } from 'node:url'
+import { includeIgnoreFile } from '@eslint/compat'
+import { defineConfig } from 'eslint/config'
+import globals from 'globals'
+import ts from 'typescript-eslint'
+import svelteConfig from './svelte.config.js'
 
-const gitignorePath = path.resolve(import.meta.dirname, '.gitignore');
+const gitignorePath = fileURLToPath(new URL('./.gitignore', import.meta.url))
+
+import isoftSvelteConfig from '@isoftdata/eslint-config-svelte'
 
 export default defineConfig(
 	includeIgnoreFile(gitignorePath),
-	js.configs.recommended,
-	ts.configs.recommended,
-	svelte.configs.recommended,
-	prettier,
-	svelte.configs.prettier,
+	...isoftSvelteConfig,
 	{
-		languageOptions: { globals: { ...globals.browser, ...globals.node } },
+		files: ['**/*.js', '**/*.cjs', '**/*.mjs', '**/*.jsx'],
+		languageOptions: {
+			parserOptions: {
+				projectService: false,
+			},
+		},
+		extends: [ts.configs.disableTypeChecked],
+	},
+	{
+		languageOptions: {
+			globals: { ...globals.browser, ...globals.node },
+		},
 		rules: {
 			// typescript-eslint strongly recommend that you do not use the no-undef lint rule on TypeScript projects.
 			// see: https://typescript-eslint.io/troubleshooting/faqs/eslint/#i-get-errors-from-the-no-undef-rule-about-global-variables-not-being-defined-even-though-there-are-no-typescript-errors
-			'no-undef': 'off'
-		}
+			'no-undef': 'off',
+		},
 	},
 	{
 		files: ['**/*.svelte', '**/*.svelte.ts', '**/*.svelte.js'],
@@ -32,13 +38,25 @@ export default defineConfig(
 				projectService: true,
 				extraFileExtensions: ['.svelte'],
 				parser: ts.parser,
-				svelteConfig
-			}
-		}
+				svelteConfig,
+			},
+		},
 	},
 	{
-		// Override or add rule settings here, such as:
-		// 'svelte/button-has-type': 'error'
-		rules: {}
-	}
-);
+		rules: {
+			'@typescript-eslint/prefer-promise-reject-errors': 'off',
+			// Force script tags to use TypeScript
+			'svelte/block-lang': [
+				'error',
+				{
+					enforceScriptPresent: true,
+					enforceStylePresent: false,
+					script: ['ts'],
+				},
+			],
+			'svelte/no-add-event-listener': 'error',
+			'svelte/no-ignored-unsubscribe': 'error',
+			'svelte/no-inspect': 'off',
+		},
+	},
+)
